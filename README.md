101afc4fefa0cb4fc372091266d930099c981bf0
enamulhaque <enamulhaque028@gmail.com>
2020-01-17 19:19:34 +0600

    created and setup full environment for testing

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-### Usage
-```
-git clone https://github.com/enamulhaque028/Bongobd_test.git
-cd Bongobd_test
-python bongo_test.py
-
-```
 ### Workspace(in the case of author)
 ```
 OS: Ubuntu 19.10
 Lannguage: Python
 Testing tool/framework: Selenium
 Browser: Chrome, Version 79.0.3945.117 (Official Build) (64-bit) 
+```
+### Selinium installation guideline
+
+Follow the (guideline)[https://selenium.dev/documentation/en/selenium_installation/]
+
+### Usage
+```
+git clone https://github.com/enamulhaque028/Bongobd_test.git
+cd Bongobd_test
+python bongo_test.py
---
+++ bongo_test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome("chromedriver")
+
+browser.maximize_window()
+
+browser.get("https://www.bongobd.com/")
+timeout = 5
+try:
+    television_selector = "a[href='/channel/channel-24']"
+    # television_selector = ".jss442"
+
+    element_present = EC.presence_of_element_located(
+        (By.CSS_SELECTOR, television_selector))
+    WebDriverWait(browser, timeout).until(element_present)
+    elements = browser.find_elements_by_css_selector(television_selector)
+    elements[0].click()
+except TimeoutException:
+    print("Timed out waiting for page to load")
